651833dd66a07c861942cba95c7305d23fad0926
Arturo Jamaica <me@arturojamaica.com>
2021-06-13 18:34:10 +0900

    Request Airdrop

---
+++ Sources/Solana/RPCMethods/requestAirdrop/requestAirdrop.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+extension Solana {
+    func requestAirdrop(account: String, lamports: UInt64, commitment: Commitment? = nil, onComplete: @escaping(Result<String, Error>)->()) {
+        request(parameters: [account, lamports, RequestConfiguration(commitment: commitment)]) { (result:Result<String, Error>) in
+            switch result {
+            case .success(let hash):
+                onComplete(.success(hash))
+            case .failure(let error):
+                onComplete(.failure(error))
+            }
+        }
+    }
+}
