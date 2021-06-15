c5fb201efc75c5cd7902b007d5d729b377606646
Arturo Jamaica <me@arturojamaica.com>
2021-06-15 22:33:03 +0900

    Fix sendSPLTokens

--- Sources/Solana/Actions/sendSPLTokens/sendSPLTokens.swift
+++ Sources/Solana/Actions/sendSPLTokens/sendSPLTokens.swift
@@ -1,5 +1,4 @@
 import Foundation
-import RxSwift
 
 extension Solana {
     public func sendSPLTokens(
@@ -7,13 +6,80 @@ extension Solana {
         decimals: Decimals,
         from fromPublicKey: String,
         to destinationAddress: String,
-        amount: UInt64
-    ) -> Single<TransactionID> {
+        amount: UInt64,
+        onComplete: @escaping (Result<TransactionID, Error>) -> ()
+    ) {
         guard let account = self.accountStorage.account else {
-            return .error(SolanaError.unauthorized)
+            return onComplete(.failure(SolanaError.unauthorized))
         }
         
-        return findSPLTokenDestinationAddress(
+        findSPLTokenDestinationAddress(
+            mintAddress: mintAddress,
+            destinationAddress: destinationAddress
+        ) { splTokeenAddressResult in
+            switch splTokeenAddressResult {
+            case .success((let destination, let isUnregisteredAsocciatedToken)):
+                let toPublicKey = destination
+                
+                // catch error
+                guard fromPublicKey != toPublicKey.base58EncodedString else {
+                    onComplete(.failure(SolanaError.invalidPublicKey))
+                    return
+                }
+                
+                guard let fromPublicKey = try? PublicKey(string: fromPublicKey) else {
+                    onComplete(.failure( SolanaError.invalidPublicKey))
+                    return
+                }
+                var instructions = [TransactionInstruction]()
+                
+                // create associated token address
+                if isUnregisteredAsocciatedToken {
+                    guard let mint = try? PublicKey(string: mintAddress) else {
+                        onComplete(.failure(SolanaError.invalidPublicKey))
+                        return
+                    }
+                    guard let owner = try? PublicKey(string: destinationAddress) else {
+                        onComplete(.failure(SolanaError.invalidPublicKey))
+                        return
+                    }
+                    
+                    let createATokenInstruction = AssociatedTokenProgram.createAssociatedTokenAccountInstruction(
+                        mint: mint,
+                        associatedAccount: toPublicKey,
+                        owner: owner,
+                        payer: account.publicKey
+                    )
+                    instructions.append(createATokenInstruction)
+                }
+                
+                // send instruction
+                let sendInstruction = TokenProgram.transferInstruction(
+                    tokenProgramId: .tokenProgramId,
+                    source: fromPublicKey,
+                    destination: toPublicKey,
+                    owner: account.publicKey,
+                    amount: amount
+                )
+                
+                instructions.append(sendInstruction)
+                
+                self.serializeAndSendWithFee(instructions: instructions, signers: [account]) { transactionResult in
+                    switch transactionResult {
+                    case .success(let transaction):
+                        onComplete(.success(transaction))
+                    case .failure(let error):
+                        onComplete(.failure(error))
+                    }
+                }
+                
+            case .failure(let error):
+                onComplete(.failure(error))
+                return
+            }
+        }
+        
+        /*return findSPLTokenDestinationAddress(
             mintAddress: mintAddress,
             destinationAddress: destinationAddress
         )
@@ -63,6 +129,6 @@ extension Solana {
                 error = SolanaError.other("Wrong wallet address")
             }
             throw error
-        }
+        }*/
     }
 }
