c028fc39e35a8de3b9fe1be22cb3d5a7fddf2478
Arturo Jamaica <me@arturojamaica.com>
2021-06-18 21:04:17 +0900

    Add results for serialized and transaction compilation

--- Sources/Solana/Extensions/AccountMeta+Extensions.swift
+++ Sources/Solana/Extensions/AccountMeta+Extensions.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 extension Array where Element == Solana.Account.Meta {
-    func index(ofElementWithPublicKey publicKey: Solana.PublicKey) throws -> Int {
-        guard let index = firstIndex(where: {$0.publicKey == publicKey})
-        else {throw Solana.SolanaError.other("Could not found accountIndex")}
-        return index
+    func index(ofElementWithPublicKey publicKey: Solana.PublicKey) -> Result<Int, Error> {
+        guard let index = firstIndex(where: {$0.publicKey == publicKey}) else {
+            return .failure( Solana.SolanaError.other("Could not found accountIndex"))}
+        return .success(index)
     }
 }
