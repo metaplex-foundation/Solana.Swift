06308044d32614da2706462654113aa3f2a6511e
Arturo Jamaica <me@arturojamaica.com>
2021-07-12 14:12:08 +0900

    Remove more imports

---
+++ Sources/Solana/Vendor/PBKDF2.swift
@@ -0,0 +1,26 @@
+import Foundation
+import CommonCrypto
+
+func pbkdf2(hash: CCPBKDFAlgorithm, password: String, salt: Data, keyByteCount: Int, rounds: Int) -> Data? {
+    let passwordData = password.data(using:String.Encoding.utf8)!
+    var derivedKeyData = Data(repeating:0, count: keyByteCount)
+    let derivedKeyDataCount = derivedKeyData.count
+    let derivationStatus = derivedKeyData.withUnsafeMutableBytes { derivedKeyBytes in
+        salt.withUnsafeBytes { saltBytes in
+            CCKeyDerivationPBKDF(
+                CCPBKDFAlgorithm(kCCPBKDF2),
+                password, passwordData.count,
+                saltBytes, salt.count,
+                hash,
+                UInt32(rounds),
+                derivedKeyBytes, derivedKeyDataCount)
+        }
+    }
+    
+    if (derivationStatus != 0) {
+        print("Error: \(derivationStatus)")
+        return nil;
+    }
+        
+    return derivedKeyData
+}
