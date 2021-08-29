120dc067e1ed885a972338dab47b4274c166fbcb
Nathan Lawrence <nathan.lawrence.barrick@gmail.com>
2021-08-29 15:51:16 -0500

    Spelling fix.

--- Sources/Solana/Networking/Router/SolanaRouter.swift
+++ Sources/Solana/Networking/Router/SolanaRouter.swift
@@ -3,7 +3,7 @@ import Foundation
 /**
  An object that determins how network requests are handled in use of the Solana tools.
 
- Most use cases can prefer `NetworkingRouter` for daily us. Conform to this with a cusstom implementation to create local integration test tooling.
+ Most use cases can prefer `NetworkingRouter` for daily use. Conform to this with a cusstom implementation to create local integration test tooling.
  */
 public protocol SolanaRouter {
     func request<T>(method: HTTPMethod, bcMethod: String, parameters: [Encodable?], onComplete: @escaping (Result<T, Error>) -> Void) where T : Decodable
