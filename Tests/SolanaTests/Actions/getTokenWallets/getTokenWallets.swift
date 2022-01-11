8e6e6a23b7e2d8954c7cb312e7e346cc50268b74
hmazelier <hadrien.mazelier@supinfo.com>
2022-01-11 16:04:39 +0200

    Migrate getTokenWallets tests using async API

--- Tests/SolanaTests/Actions/getTokenWallets/getTokenWallets.swift
+++ Tests/SolanaTests/Actions/getTokenWallets/getTokenWallets.swift
@@ -1,6 +1,7 @@
 import XCTest
 import Solana
 
+@available(iOS 13.0, *)
 class getTokenWallets: XCTestCase {
     var endpoint = RPCEndpoint.devnetSolana
     var solana: Solana!
@@ -13,17 +14,16 @@ class getTokenWallets: XCTestCase {
         try solana.auth.save(account).get()
     }
     
-    func testsGetTokenWalletsParsing() {
+    func testsGetTokenWalletsParsing() throws {
         let jsonData = getFileFrom("TokenInfo/getTokenWallets")
 
-        let wallets = try! JSONDecoder().decode(Response<Rpc<[TokenAccount<AccountInfoData>]>>.self, from: jsonData)
-        XCTAssertNotNil(wallets.result!.value)
+        let wallets = try JSONDecoder().decode(Response<Rpc<[TokenAccount<AccountInfoData>]>>.self, from: jsonData)
+        XCTAssertNotNil(wallets.result?.value)
         XCTAssertNotEqual(wallets.result!.value.count, 0)
     }
     
-    func testsGetTokenWallets() {
-        let wallets = try? solana.action.getTokenWallets()?.get()
-        XCTAssertNotNil(wallets)
-        XCTAssertNotEqual(wallets!.count, 0)
+    func testsGetTokenWallets() async throws {
+        let wallets = try await solana.action.getTokenWallets()
+        XCTAssertFalse(wallets.isEmpty)
     }
 }
