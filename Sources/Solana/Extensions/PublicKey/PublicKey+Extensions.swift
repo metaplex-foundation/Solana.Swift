411ae707e47fb3e16e4de0600b506bee6730b51f
Jay Abbott <1160012+j-h-a@users.noreply.github.com>
2022-02-04 21:03:25 +0000

    Rename `programId` to `systemProgramId` and mark old name as deprecated.

--- Sources/Solana/Extensions/PublicKey/PublicKey+Extensions.swift
+++ Sources/Solana/Extensions/PublicKey/PublicKey+Extensions.swift
@@ -3,7 +3,9 @@ import Foundation
 public extension PublicKey {
     static let tokenProgramId = PublicKey(string: "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")!
     static let sysvarRent = PublicKey(string: "SysvarRent111111111111111111111111111111111")!
+    @available(*, deprecated, renamed: "systemProgramId")
     static let programId = PublicKey(string: "11111111111111111111111111111111")!
+    static let systemProgramId = PublicKey(string: "11111111111111111111111111111111")!
     static let wrappedSOLMint = PublicKey(string: "So11111111111111111111111111111111111111112")!
     static let ownerValidationProgramId = PublicKey(string: "4MNPdKu9wFMvEeZBMt3Eipfs5ovVWTJb31pEXDJAAxX5")!
     static let swapHostFeeAddress = PublicKey(string: "AHLwq66Cg3CuDJTFtwjPfwjJhifiv6rFwApQNKgX57Yg")!
