217036306ae925781a1de055c644f92b4888aeaf
coralreefer <coral@hiseas.im>
2022-03-07 14:27:29 +0800

    public TransactionInstruction:init

--- Sources/Solana/Models/TransactionInstruction.swift
+++ Sources/Solana/Models/TransactionInstruction.swift
@@ -5,7 +5,7 @@ public struct TransactionInstruction: Decodable {
     public let programId: PublicKey
     public let data: [UInt8]
 
-    init(keys: [Account.Meta], programId: PublicKey, data: [BytesEncodable]) {
+    public init(keys: [Account.Meta], programId: PublicKey, data: [BytesEncodable]) {
         self.keys = keys
         self.programId = programId
         self.data = data.bytes
