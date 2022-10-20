de18f74a5f86df962b422b9a0fc2474de23348d7
Arturo Jamaica <me@arturojamaica.com>
2022-10-20 09:51:45 -0500

    Fix pbkdf2

--- Sources/Solana/Vendor/PBKDF2.swift
+++ Sources/Solana/Vendor/PBKDF2.swift
@@ -5,19 +5,21 @@ func pbkdf2(hash: CCPBKDFAlgorithm, password: String, salt: Data, keyByteCount: 
     let passwordData = password.data(using: String.Encoding.utf8)!
     var derivedKeyData = Data(repeating: 0, count: keyByteCount)
     let derivedKeyDataCount = derivedKeyData.count
-    let derivationStatus = derivedKeyData.withUnsafeMutableBytes { derivedKeyBytes in
-        salt.withUnsafeBytes { saltBytes in
-            CCKeyDerivationPBKDF(
-                CCPBKDFAlgorithm(kCCPBKDF2),
-                password, passwordData.count,
-                saltBytes, salt.count,
-                hash,
-                UInt32(rounds),
-                derivedKeyBytes, derivedKeyDataCount)
-        }
+    let derivationStatus = derivedKeyData.withUnsafeMutableBytes { rawMutableBufferPointer in
+        let derivedKeyBytes = rawMutableBufferPointer.bindMemory(to: UInt8.self)
+        return CCKeyDerivationPBKDF(
+            CCPBKDFAlgorithm(kCCPBKDF2),
+            password,
+            passwordData.count,
+            salt.bytes,
+            salt.count,
+            hash,
+            UInt32(rounds),
+            derivedKeyBytes.baseAddress,
+            derivedKeyDataCount)
     }
 
-    if derivationStatus != 0 {
+    if derivationStatus != kCCSuccess {
         print("Error: \(derivationStatus)")
         return nil
     }
