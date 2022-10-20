37e0ef7a6776e6c4182732b1457968365160818f
Arturo Jamaica <me@arturojamaica.com>
2022-10-20 09:58:49 -0500

    Fix infered code on old machines

--- Sources/Solana/Vendor/PBKDF2.swift
+++ Sources/Solana/Vendor/PBKDF2.swift
@@ -5,7 +5,7 @@ func pbkdf2(hash: CCPBKDFAlgorithm, password: String, salt: Data, keyByteCount: 
     let passwordData = password.data(using: String.Encoding.utf8)!
     var derivedKeyData = Data(repeating: 0, count: keyByteCount)
     let derivedKeyDataCount = derivedKeyData.count
-    let derivationStatus = derivedKeyData.withUnsafeMutableBytes { rawMutableBufferPointer in
+    let derivationStatus = derivedKeyData.withUnsafeMutableBytes { (rawMutableBufferPointer: UnsafeMutableRawBufferPointer) -> Int32 in
         let derivedKeyBytes = rawMutableBufferPointer.bindMemory(to: UInt8.self)
         return CCKeyDerivationPBKDF(
             CCPBKDFAlgorithm(kCCPBKDF2),
