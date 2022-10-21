464eb1a457c470abcb681d0a8f05faffc31f660e
Arturo Jamaica <me@arturojamaica.com>
2022-10-21 11:34:18 -0500

    getMultipleAccounts

--- Sources/Solana/Api/getMultipleAccounts/getMultipleAccounts.swift
+++ Sources/Solana/Api/getMultipleAccounts/getMultipleAccounts.swift
@@ -1,6 +1,12 @@
 import Foundation
 
 public extension Api {
+    /// Returns the account information for a list of PublicKey.
+    /// 
+    /// - Parameters:
+    ///   - pubkeys: An array of Pubkeys to query, as base-58 encoded strings (up to a maximum of 100).
+    ///   - decodedTo: Object from which the data value will be mapped. Must be BufferLayout implementation
+    ///   - onComplete: The result object of Arrays of BufferInfo<T>. Where T is the decodedTo object. 
     func getMultipleAccounts<T: BufferLayout>(pubkeys: [String],
                                               decodedTo: T.Type,
                                               onComplete: @escaping (Result<[BufferInfo<T>?], Error>) -> Void) {
@@ -23,6 +29,12 @@ public extension Api {
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
 public extension Api {
+    /// Returns the account information for a list of PublicKey.
+    /// 
+    /// - Parameters:
+    ///   - pubkeys: An array of Pubkeys to query, as base-58 encoded strings (up to a maximum of 100).
+    ///   - decodedTo: Object from which the data value will be mapped. Must be BufferLayout implementation
+    /// - Returns: The result object of Arrays of BufferInfo<T>. Where T is the decodedTo object. 
     func getMultipleAccounts<T: BufferLayout>(pubkeys: [String], decodedTo: T.Type) async throws -> [BufferInfo<T>?] {
         try await withCheckedThrowingContinuation { c in
             self.getMultipleAccounts(pubkeys: pubkeys, decodedTo: decodedTo, onComplete: c.resume(with:))
