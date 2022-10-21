bbe412bcfd3bfe9f05015070ef40a005f4650b46
Arturo Jamaica <me@arturojamaica.com>
2022-10-21 11:45:25 -0500

    getSlot

--- Sources/Solana/Api/getSignatureStatuses/getSignatureStatuses.swift
+++ Sources/Solana/Api/getSignatureStatuses/getSignatureStatuses.swift
@@ -1,11 +1,13 @@
 import Foundation
 
 public extension Api {
-    /// Returns the statuses of a list of signatures. Unless the searchTransactionHistory configuration parameter is included, this method only searches the recent status cache of signatures, which retains statuses for all active slots plus MAX_RECENT_BLOCKHASHES rooted slots.
+    /// Returns the statuses of a list of signatures. Unless the searchTransactionHistory
+    ///  configuration parameter is included, this method only searches the recent status
+    ///  cache of signatures, which retains statuses for all active slots plus MAX_RECENT_BLOCKHASHES rooted slots.
     /// - Parameters:
-    ///   - pubkeys: 
-    ///   - configs: 
-    ///   - onComplete: 
+    ///   - pubkeys: An array of transaction signatures to confirm, as base-58 encoded strings (up to a maximum of 256)
+    ///   - configs: Configuration object 
+    ///   - onComplete: An Result object of an array of TransactionStatus objects.
     func getSignatureStatuses(pubkeys: [String], configs: RequestConfiguration? = nil, onComplete: @escaping (Result<[SignatureStatus?], Error>) -> Void) {
         router.request(parameters: [pubkeys, configs]) { (result: Result<Rpc<[SignatureStatus?]?>, Error>) in
             switch result {
@@ -25,6 +27,13 @@ public extension Api {
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
 public extension Api {
+    /// Returns the statuses of a list of signatures. Unless the searchTransactionHistory
+    ///  configuration parameter is included, this method only searches the recent status
+    ///  cache of signatures, which retains statuses for all active slots plus MAX_RECENT_BLOCKHASHES rooted slots.
+    /// - Parameters:
+    ///   - pubkeys: An array of transaction signatures to confirm, as base-58 encoded strings (up to a maximum of 256)
+    ///   - configs: Configuration object 
+    /// - Returns: An Result object of an array of TransactionStatus objects.
     func getSignatureStatuses(pubkeys: [String], configs: RequestConfiguration? = nil) async throws -> [SignatureStatus?] {
         try await withCheckedThrowingContinuation { c in
             self.getSignatureStatuses(pubkeys: pubkeys, configs: configs, onComplete: c.resume(with:))
