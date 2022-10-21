b96263eed765454bd1c1337e501512da3837e921
Arturo Jamaica <me@arturojamaica.com>
2022-10-21 12:20:55 -0500

    requestAirdrop

--- Sources/Solana/Api/requestAirdrop/requestAirdrop.swift
+++ Sources/Solana/Api/requestAirdrop/requestAirdrop.swift
@@ -1,6 +1,13 @@
 import Foundation
 
 public extension Api {
+    /// Requests an airdrop of lamports to a Pubkey
+    /// 
+    /// - Parameters:
+    ///   - account: Pubkey of account to receive lamports, as base-58 encoded string
+    ///   - lamports: lamports, as a UInt64
+    ///   - commitment: The commitment describes how finalized a block is at that point in time. (finalized, confirmed, processed)
+    ///   - onComplete: The result object of Transaction Signature of airdrop, as base-58 encoded string
     func requestAirdrop(account: String, lamports: UInt64, commitment: Commitment? = nil, onComplete: @escaping(Result<String, Error>) -> Void) {
         router.request(parameters: [account, lamports, RequestConfiguration(commitment: commitment)]) { (result: Result<String, Error>) in
             switch result {
@@ -16,6 +23,13 @@ public extension Api {
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
 public extension Api {
+    /// Requests an airdrop of lamports to a Pubkey
+    /// 
+    /// - Parameters:
+    ///   - account: Pubkey of account to receive lamports, as base-58 encoded string
+    ///   - lamports: lamports, as a UInt64
+    ///   - commitment: The commitment describes how finalized a block is at that point in time. (finalized, confirmed, processed)
+    /// - Returns: The Transaction Signature of airdrop, as base-58 encoded string
     func requestAirdrop(account: String, lamports: UInt64, commitment: Commitment? = nil) async throws -> String {
         try await withCheckedThrowingContinuation { c in
             self.requestAirdrop(account: account, lamports: lamports, commitment: commitment, onComplete: c.resume(with:))
