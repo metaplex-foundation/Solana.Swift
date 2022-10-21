65269babb32422f8234be67926691ff8e352ed70
Arturo Jamaica <me@arturojamaica.com>
2022-10-21 11:41:47 -0500

    getRecentPerformanceSamples

--- Sources/Solana/Api/getRecentPerformanceSamples/getRecentPerformanceSamples.swift
+++ Sources/Solana/Api/getRecentPerformanceSamples/getRecentPerformanceSamples.swift
@@ -1,6 +1,11 @@
 import Foundation
 
 public extension Api {
+    /// Returns a list of recent performance samples, in reverse slot order. Performance samples are taken every 60 seconds and include the number of transactions and slots that occur in a given time window.
+    /// 
+    /// - Parameters:
+    ///   - limit: number of samples to return (maximum 720)
+    ///   - onComplete: The result of an array PerformanceSample(numSlots: UInt64, numTransactions: UInt64, samplePeriodSecs: UInt slot: UInt64) 
     func getRecentPerformanceSamples(limit: UInt64, onComplete: @escaping(Result<[PerformanceSample], Error>) -> Void) {
         router.request(parameters: [limit]) { (result: Result<[PerformanceSample], Error>) in
             switch result {
@@ -16,6 +21,11 @@ public extension Api {
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
 public extension Api {
+    /// Returns a list of recent performance samples, in reverse slot order. Performance samples are taken every 60 seconds and include the number of transactions and slots that occur in a given time window.
+    /// 
+    /// - Parameters:
+    ///   - limit: number of samples to return (maximum 720)
+    /// - Returns: The result of an array PerformanceSample(numSlots: UInt64, numTransactions: UInt64, samplePeriodSecs: UInt slot: UInt64) 
     func getRecentPerformanceSamples(limit: UInt64) async throws -> [PerformanceSample] {
         try await withCheckedThrowingContinuation { c in
             self.getRecentPerformanceSamples(limit: limit, onComplete: c.resume(with:))
