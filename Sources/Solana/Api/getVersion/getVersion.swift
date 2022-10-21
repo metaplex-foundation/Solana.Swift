bf7c6ac92615932fedb7c5ebc8151af776a71cd0
Arturo Jamaica <me@arturojamaica.com>
2022-10-21 12:15:41 -0500

    getVersion

--- Sources/Solana/Api/getVersion/getVersion.swift
+++ Sources/Solana/Api/getVersion/getVersion.swift
@@ -1,6 +1,9 @@
 import Foundation
 
 public extension Api {
+    /// Returns the current solana versions running on the node
+    /// 
+    /// - Parameter onComplete: The result object of Version(solana-core, software version of solana-core. feature-set, unique identifier of the current software's feature set)
     func getVersion(onComplete: @escaping(Result<Version, Error>) -> Void) {
         router.request { (result: Result<Version, Error>) in
             switch result {
@@ -16,6 +19,9 @@ public extension Api {
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
 public extension Api {
+    /// Returns the current solana versions running on the node
+    /// 
+    /// - Returns: The result object of Version(solana-core, software version of solana-core. feature-set, unique identifier of the current software's feature set)
     func getVersion() async throws -> Version {
         try await withCheckedThrowingContinuation { c in
             self.getVersion(onComplete: c.resume(with:))
