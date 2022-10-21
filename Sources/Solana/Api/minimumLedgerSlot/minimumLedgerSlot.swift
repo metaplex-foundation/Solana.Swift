301a31bcadcbe3a37f0960550e6138e37723e337
Arturo Jamaica <me@arturojamaica.com>
2022-10-21 12:19:49 -0500

    minimumLedgerSlot

--- Sources/Solana/Api/minimumLedgerSlot/minimumLedgerSlot.swift
+++ Sources/Solana/Api/minimumLedgerSlot/minimumLedgerSlot.swift
@@ -1,6 +1,10 @@
 import Foundation
 
 public extension Api {
+    /// Returns the lowest slot that the node has information about in its ledger.
+    ///  This value may increase over time if the node is configured to purge older ledger data
+    /// 
+    /// - Parameter onComplete: Minimum ledger slot UInt64
     func minimumLedgerSlot(onComplete: @escaping (Result<UInt64, Error>) -> Void) {
         router.request { (result: Result<UInt64, Error>) in
             switch result {
@@ -16,6 +20,10 @@ public extension Api {
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
 public extension Api {
+    /// Returns the lowest slot that the node has information about in its ledger.
+    ///  This value may increase over time if the node is configured to purge older ledger data
+    /// 
+    /// - Returns: Minimum ledger slot UInt64
     func minimumLedgerSlot() async throws -> UInt64 {
         try await withCheckedThrowingContinuation { c in
             self.minimumLedgerSlot(onComplete: c.resume(with:))
