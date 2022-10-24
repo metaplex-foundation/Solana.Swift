9516686be73fb850df0dab013b8532323d822a03
Arturo Jamaica <me@arturojamaica.com>
2022-10-25 02:27:22 +0900

    Update Sources/Solana/Api/getProgramAccounts/getProgramAccounts.swift
    Co-authored-by: mike-metaplex <111764486+mike-metaplex@users.noreply.github.com>

--- Sources/Solana/Api/getProgramAccounts/getProgramAccounts.swift
+++ Sources/Solana/Api/getProgramAccounts/getProgramAccounts.swift
@@ -54,7 +54,7 @@ public extension Api {
     ///   - publicKey: Pubkey of program, as base-58 encoded string
     ///   - configs: Configuration object containing 
     ///   - decodedTo: Object from which the data value will be mapped. Must be `BufferLayout` implementation
-    /// - Returns: Arrays of ProgramAccount<T>. Where T is the decodedTo object. 
+    /// - Returns: Arrays of ProgramAccount<T>. Where T is the decodedTo object
     func getProgramAccounts<T: BufferLayout>(
         publicKey: String,
         configs: RequestConfiguration? = RequestConfiguration(encoding: "base64"),
