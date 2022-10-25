77b48fb915f37ec5faa7fc82507cf28cdce7e99e
Arturo Jamaica <me@arturojamaica.com>
2022-10-25 15:29:30 -0500

    Update beet 1.0.2

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(name: "TweetNacl", url: "https://github.com/bitmark-inc/tweetnacl-swiftwrap.git", from: "1.0.2"),
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.0"),
-        .package(name: "Beet", url: "https://github.com/metaplex-foundation/beet-swift.git", from: "1.0.1"),
+        .package(name: "Beet", url: "https://github.com/metaplex-foundation/beet-swift.git", from: "1.0.2"),
         .package(name: "secp256k1", url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.7"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
