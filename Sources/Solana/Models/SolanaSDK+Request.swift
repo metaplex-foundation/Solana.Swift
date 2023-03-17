1b545c11dae354b5f51b8afa7d045c17c84d26d8
Andy Boyd <andrew.boyd.ni@gmail.com>
2023-03-17 11:23:07 +0000

    adding skip preflight functionality to API requests

--- Sources/Solana/Models/SolanaSDK+Request.swift
+++ Sources/Solana/Models/SolanaSDK+Request.swift
@@ -35,6 +35,7 @@ public struct RequestConfiguration: Encodable {
     public let limit: Int?
     public let before: String?
     public let until: String?
+    public let skipPreflight: Bool?
 
     public init?(
         commitment: Commitment? = nil,
@@ -43,7 +44,8 @@ public struct RequestConfiguration: Encodable {
         filters: [[String: EncodableWrapper]]? = nil,
         limit: Int? = nil,
         before: String? = nil,
-        until: String? = nil
+        until: String? = nil,
+        skipPreflight: Bool? = nil
     ) {
         if commitment == nil
             && encoding == nil
@@ -51,7 +53,8 @@ public struct RequestConfiguration: Encodable {
             && filters == nil
             && limit == nil
             && before == nil
-            && until == nil {
+            && until == nil
+            && skipPreflight == nil {
             return nil
         }
         self.commitment = commitment
@@ -61,6 +64,7 @@ public struct RequestConfiguration: Encodable {
         self.limit = limit
         self.before = before
         self.until = until
+        self.skipPreflight = skipPreflight
     }
 }
 
